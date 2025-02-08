00890ef676b35ca8bbb7f98c65d5f170530be5c9
Logan Besecker <lbesecker195@gmail.com>
2025-02-07 19:53:29 -0800

    580085

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,65 @@
 var express = require('express');
 var router = express.Router();
 
+
+function intDivision(x, y) {
+	return Math.floor(x/y);
+}
+
+function isPalindrom(x) {
+	console.log('[START] isPalandrom()')
+	var num;
+	if(typeof x == String) {
+		num = x;
+	} else {
+		num = x.toString();
+	}
+
+	console.log(`typeof num: ${typeof num}`)
+	console.log(`x: ${x}`)
+	if (num.length > 1) {
+		console.log(`num[0]: ${num[0]}`)
+		console.log(`num[-1]: ${num[num.length - 1]}`)
+
+		if (num[0] == num[num.length - 1]) {
+			console.log("(num[0] == num[-1])");
+
+			console.log(`num.length: ${num.length}`)
+			var newX = num.substring(1, num.length - 1);
+			console.log('newX: ' + newX)
+
+			return isPalindrom(newX)
+		} else {
+			return false;
+		}
+	} else {
+		return true
+	}
+
+	console.log('[END] isPalandrom()')
+}
+
+
 /* GET home page. */
-router.get('/:foo/squared', function(req, res, next) {
-  res.json({foo: req.params.foo * req.params.foo});
+router.get('/:foo', function(req, res, next) {
+	var palindroms = []
+	for (i = 100; i < 1000; i++ ) {
+		for (j = 100; j < 1000; j++) {
+			let num = i*j;
+			if (isPalindrom(num)) {
+				palindroms = palindroms + num;
+			}
+		}
+	}
+
+	console.log("palindroms:")
+	console.log(palindroms)
+
+	var maxPalindrom = Math.max(...palindroms);
+
+	console.log("maxPalindrom:")
+	console.log(maxPalindrom)
+  res.json({foo: maxPalindrom});
 });
 
 module.exports = router;
